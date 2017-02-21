488e409f8351f0512e87a3de670396f68fe3e5ee
Filippo Sestini <sestini.filippo@gmail.com>
2017-02-21 21:55:21 +0100

    Add Main

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,17 @@
 module Main where
 
-import Lib
+import Proof
+import Parser
+import System.IO
 
 main :: IO ()
-main = someFunc
+main = do
+  putStr "Enter sequent: "
+  hFlush stdout
+  line <- getLine
+  case parseSequent line of
+    Left err -> putStrLn $ "Error: " ++ (show err)
+    Right sequent -> do pt <- prove sequent
+                        putStrLn "No more goals. Proof tree:"
+                        putStrLn . show $ pt
+                        main
