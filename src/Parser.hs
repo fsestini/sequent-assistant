3b45ef4b5f71e06164b92b4e7200c416ad8888de
Filippo Sestini <sestini.filippo@gmail.com>
2017-03-02 22:08:45 +0100

    Develop parser

--- src/Parser.hs
+++ src/Parser.hs
@@ -4,9 +4,13 @@ module Parser where
 
 import Formula
 import Substitution
+
 import Text.Parsec.String
 import Text.Parsec.Char
+import Text.Parsec.Prim hiding (try)
+import Text.Parsec.Combinator
 
+import Control.Monad.Reader
 import System.IO
 import Control.Monad
 import Text.ParserCombinators.Parsec
@@ -14,8 +18,17 @@ import Text.ParserCombinators.Parsec.Expr
 import Text.ParserCombinators.Parsec.Language
 import qualified Text.ParserCombinators.Parsec.Token as Token
 
-functionSymbols :: [String]
-functionSymbols = ["s", "z"]
+type TParser a = ReaderT (Theory String) Parser a
+
+liftBinary :: (Parser a -> Parser b -> Parser c) -> TParser a -> TParser b -> TParser c
+liftBinary f tp1 tp2 = do
+  theory <- getTheory
+  lift (f (toParser tp1 theory) (toParser tp2 theory))
+
+liftUnary :: (Parser a -> Parser b) -> TParser a -> TParser b
+liftUnary f tp = do
+  theory <- getTheory
+  lift (f (toParser tp theory))
 
 languageDef =
   emptyDef
@@ -39,16 +52,16 @@ lexer = Token.makeTokenParser languageDef
 
 identifier = Token.identifier lexer
 reserved = Token.reserved lexer
+
 reservedOp = Token.reservedOp lexer
-parens     = Token.parens     lexer 
-whiteSpace = Token.whiteSpace lexer 
+
+parens :: Parser a -> Parser a
+parens = Token.parens     lexer
+
+whiteSpace = Token.whiteSpace lexer
+
+comma :: Parser String
 comma = Token.comma lexer
-
-mainParser :: Parser (Formula String)
-mainParser = whiteSpace >> formula
-
-formula :: Parser (Formula String)
-formula = parens formula
 
 forallTransformer :: Parser (Formula String -> Formula String)
 forallTransformer = do
@@ -63,48 +76,91 @@ existsTransformer = do
   return $ abstractExists x
 
 operators =
-  [ [ Prefix (reservedOp "not" >> return (Not))
+  [ [ Prefix (notOp >> return (Not))
     , Prefix forallTransformer
     , Prefix existsTransformer]
-  , [ Infix (reservedOp "and" >> return (Conj)) AssocLeft
-    , Infix (reservedOp "or" >> return (Disj)) AssocLeft
+  , [ Infix (andOp >> return (Conj)) AssocLeft
+    , Infix (orOp >> return (Disj)) AssocLeft
     ]
-  , [Infix (reservedOp "->" >> return (Impl)) AssocLeft]
+  , [Infix (implOp >> return (Impl)) AssocLeft]
   ]
+  where
+    notOp = Token.reservedOp lexer "not"
+    andOp = Token.reservedOp lexer "and"
+    orOp = Token.reservedOp lexer "or"
+    implOp = Token.reservedOp lexer "->"
 
-expr :: Parser (Formula String)
-expr = buildExpressionParser operators atom
+toParser :: TParser a -> Theory String -> Parser a
+toParser tp t = runReaderT tp t
 
-atom :: Parser (Formula String)
-atom  =  parens expr
-     <|> (reserved "true" >> return Top)
-     <|> (reserved "false" >> return Bottom)
-     <|> Predicate <$> identifier
-                   <*> (try (parens (sepBy1 parseTerm comma)) <|> return [])
+getTheory :: TParser (Theory String)
+getTheory = ask
 
-parseTerm :: Parser (Term String) 
-parseTerm = do
-  ident <- identifier
-  if elem ident functionSymbols
+string' :: String -> TParser String
+string' = lift . string
+
+formulaParser :: TParser (Formula String)
+formulaParser = lift whiteSpace >> expr
+
+expr :: TParser (Formula String)
+expr = do
+  theory <- getTheory
+  lift $ buildExpressionParser operators (toParser atom theory)
+
+infixl 3 <||>
+(<||>) = liftBinary (<|>)
+parens' = liftUnary parens
+try' = liftUnary try
+sepBy1' = liftBinary sepBy1
+many1' = liftUnary many1
+alphaNum' :: TParser Char
+alphaNum' = lift alphaNum
+
+ppred :: String -> [Term String] -> Formula String
+ppred str args = if str == "eq"
+                    then Equality (head args) (head (tail args))
+                    else Predicate str args
+
+atom :: TParser (Formula String)
+atom =  parens' formulaParser
+    <||> (lift (reserved "true") >> return Top)
+    <||> (lift (reserved "false") >> return Bottom)
+    <||> ppred <$> lift identifier
+               <*> (try' (parens' (sepBy1' (termParser) (lift comma)))
+                          <||> return [])
+
+
+
+termParser :: TParser (Term String) 
+termParser = do
+  ident <- lift identifier
+  theory <- ask
+  if elem ident (functionSymbols theory)
      then do
-       args <- try (parens (sepBy1 parseTerm comma)) <|> return []
+       args <- try' (parens' (sepBy1' termParser (lift comma))) <||> return []
        return $ Func ident args
      else return . Free $ ident
 
-parseString :: String -> Formula String
-parseString str =
-  case parse expr "" str of
+parseTerm :: Theory String -> String -> Term String
+parseTerm theory str =
+  case parse (toParser termParser theory) "" str of
+    Left e -> error $ show e
+    Right t -> t
+
+parseFormula :: Theory String -> String -> Formula String
+parseFormula theory str =
+  case parse (toParser formulaParser theory) "" str of
     Left e -> error $ show e
     Right f -> f
 
-sequentParser :: Parser (Sequent String)
+sequentParser :: TParser (Sequent String)
 sequentParser = do
-  left <- sepBy1 expr comma
-  whiteSpace
-  string "|-"
-  whiteSpace
-  right <- sepBy1 expr comma
+  left <- sepBy1' formulaParser (lift comma)
+  lift whiteSpace
+  lift $ string "|-"
+  lift whiteSpace
+  right <- sepBy1' formulaParser (lift comma)
   return (S left right)
 
-parseSequent :: String -> Either ParseError (Sequent String)
-parseSequent = parse sequentParser ""
+parseSequent :: Theory String -> String -> Either ParseError (Sequent String)
+parseSequent theory = parse (toParser sequentParser theory) ""
